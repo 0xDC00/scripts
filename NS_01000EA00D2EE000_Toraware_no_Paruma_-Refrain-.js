098d150de9ce0021324026164126aad3b7dafc9b
0xDC00 <c0d3rR1pp3r@gmail.com>
2022-02-13 11:56:31 +0700

    [Toraware no Paruma -Refrain-] New hooks

--- NS_01000EA00D2EE000_Toraware_no_Paruma_-Refrain-.js
+++ NS_01000EA00D2EE000_Toraware_no_Paruma_-Refrain-.js
@@ -14,14 +14,16 @@ const mainHandler = trans.send(handler, '250+');
 
 setHook({
     '1.0.0': {
-        0x84ff9bc: mainHandler, // text x0
-        0x84ff994: mainHandler, // name x0 (0x8696744 (miss), 0x84ff994 (html tag))
-        0x8d2aca4: mainHandler, // choice
+        0x8697300: mainHandler, // text x1
+        0x86f43c0: mainHandler, // name x0
+        0x8d2aca4: mainHandler, // choice x0
+        0x84b04c8: mainHandler, // alert x0
+        0x84b725c: mainHandler, // prompt x0
     }
 }[globalThis.gameVer ?? gameVer]);
 
 function handler(regs) {
-    const index = 0;
+    const index = this.context.pc == 0x8697300 ? 1 : 0;
     const address = regs[index].value;
     console.log('onEnter');
     //console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
@@ -31,11 +33,10 @@ function handler(regs) {
     let s = null;
     if (len > 0) {
         s = address.add(0x14).readUtf16String(len);
+        s = s.replace(/\n+/g, ' ');  // single line
+        s = s.replace(/\<PL_Namae\>/g, '???'); // name
+        s = s.replace(/\<chiaki_washa\>/g, 'chiaki_washa');
         s = s.replace(/<.+?>/g, ''); // html tag
-        if (s !== 'none') {
-            s = s.replace(/\n+/g, ' ');  // single line
-        }
-        else s = null; // skip empty name
     }
 
     return s;
