c42b5f4d45eb538e821d7ef5094d628a3be95b94
0xDC00 <c0d3rR1pp3r@gmail.com>
2022-02-26 20:08:11 +0700

    Add NS_0100D12014FC2000_Yuru Camp_Have_a_nice_day.js (thanks darkmans11#5781)

---
+++ NS_0100D12014FC2000_Yuru Camp_Have_a_nice_day.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         [0100D12014FC2000] Yuru Camp△ - Have a Nice Day!
+// @version      0.1 - 1.0.0
+// @author       [Darkmans]
+// @description  Yuzu
+// * MAGES. GAME
+// * Unity (il2cpp)
+//
+// https://vndb.org/v30937
+// ==/UserScript==
+const gameVer = '1.0.0';
+
+const { setHook } = require('./libYuzu.js');
+
+const mainHandler = trans.send(handler, "200+");
+
+setHook({
+    '1.0.0': {
+        0x96D03F8: mainHandler, // dialog / backlog
+    }
+}[globalThis.gameVer ?? gameVer]);
+
+function handler(regs) {
+    const address = regs[0].value;
+    console.log('onEnter');
+
+    /* processString */
+    const len = address.add(0x10).readU32() * 2;
+    let s = address.add(0x14).readUtf16String(len);
+    s = s.replace(/\n+|(\\n)+/g, ' ');
+
+    return s;
+}
