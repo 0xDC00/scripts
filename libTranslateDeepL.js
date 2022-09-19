94f20df50161ee6db2ae26ef05ea47361e5736c0
0xDC00 <c0d3rR1pp3r@gmail.com>
2022-09-20 01:23:46 +0700

    DeepL: tweaks

--- libTranslateDeepL.js
+++ libTranslateDeepL.js
@@ -16,7 +16,11 @@
         let timestamp = Date.now();
         timestamp = (timestamp - (timestamp % n)) + n; // from translater_late
         str = JSON.stringify(str);
-        const body = `{"params":{"texts":[{"text":${str},"requestAlternatives":3}],"splitting":"newlines","commonJobParams":{"regionalVariant":${regionVar},"wasSpoken":false},"lang":{"target_lang":"${targetLanguage}","source_lang_user_selected":"${sourceLanguage}"},"timestamp":${timestamp}},"id":${id},"jsonrpc":"2.0","method"${((id + 3) % 13 === 0 || (id + 5) % 29 === 0) ? " : " : ": "}"LMT_handle_texts"}`;
+        const body = `{"params":{"texts":[{"text":${str},"requestAlternatives":0}],"splitting":"newlines","commonJobParams":{"regionalVariant":${regionVar},"wasSpoken":false},"lang":{"target_lang":"${targetLanguage}","source_lang_user_selected":"${sourceLanguage}"},"timestamp":${timestamp}},"id":${id},"jsonrpc":"2.0","method"${((id + 3) % 13 === 0 || (id + 5) % 29 === 0) ? " : " : ": "}"LMT_handle_texts"}`;
+        return post(body);
+    }
+
+    function post(body, again) {
         return fetch("https://www2.deepl.com/jsonrpc", {
             headers: {
                 'content-type': 'application/json; charset=utf-8'
@@ -24,15 +28,26 @@
             method: "POST",
             body: body,
             credentials: 'include'
-        }).then(r => r.json()).then(v => {
-            const texts = v.result.texts;
-            if (texts.length !== 0) {
-                return texts[0].text;
-            }
-            else {
-                // error, do nothing
-            }
-        });
+        }).then(r => r.json())
+            .then(async v => {
+                if (v.result !== undefined) {
+                    return v.result.texts[0].text;
+                }
+                else {
+                    if (again === true) {
+                        return v.error.message;
+                    }
+                    else {
+                        if (v.error !== undefined && v.error.code === 1042902) {
+                            await cookieStore.delete({ url: 'https://www.deepl.com/' });
+                            return post(body, true);
+                        }
+                        else {
+                            return 'ERROR';
+                        }
+                    }
+                }
+            });
     }
 
     function setSrc(v) {
