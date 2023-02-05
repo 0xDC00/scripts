f66ba0d7a3e2e0aabd6ca5eec9007beeade25052
0xDC00 <c0d3rR1pp3r@gmail.com>
2023-02-06 00:41:01 +0700

    Add Nil Adminari no Tenbin Irodori Nadeshiko (Switch, 01002BB00A662000)
    Thanks Koukdw#4253

---
+++ NS_01002BB00A662000_Nil_Adminari_no_Tenbin_Irodori_Nadeshiko.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         [01002BB00A662000] Nil Adminari no Tenbin Irodori Nadeshiko
+// @version      1.0.0 (base)
+// @author       Koukdw
+// @description  
+// * Otomate
+// * Idea Factory (アイディアファクトリー)
+// *
+// ==/UserScript==
+const gameVer = '1.0.0';
+
+const { setHook } = require('./libYuzu.js');
+
+const mainHandler = trans.send(handler, '200+'); // join 200ms
+
+setHook({
+    '1.0.0': {
+        0x807C3E8: mainHandler.bind_(null, 0, "name"), // name slow but in order (work on both games)
+        0x807c23c: mainHandler.bind_(null, 0, "dialogue"), // dialogue slow but in order (work on both games)
+    }
+}[globalThis.gameVer ?? gameVer]);
+
+function handler(regs, index, hookname) {
+    const reg = regs[index];
+    // Filter garbage calls causing errors
+    if (reg.vm < 0x8000000 || reg.vm > 0x9999999) return null;
+
+    console.log('onEnter: ' + hookname);
+    const address = reg.value;
+    //console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
+
+    /* processString */
+    let s = address.readShiftJisString();
+    // print rubi
+    const rubis = s.matchAll(/(#Ruby\[)([^,]+).([^\]]+)./g);
+    for (const rubi of rubis) {
+        console.log('rubi', rubi[3]);
+        console.log('rube', rubi[2]);
+    }
+    // remove rubi
+    s = s.replace(/(#Ruby\[)([^,]+).([^\]]+)./g, '$2');
+
+    s = s.replace(/(#n)+/g, ' ') // Single line
+        .replace(/(#[A-Za-z]+\[(\d*[.])?\d+\])+/g, ''); // Remove controls
+    // #Ruby[森恒犀鳥,もりつねさいちょう]
+    // #n
+    return s;
+}
