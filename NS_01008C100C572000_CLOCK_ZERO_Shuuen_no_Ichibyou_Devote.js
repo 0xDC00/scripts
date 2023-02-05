a55fc4833de23bb36559d057477ef7aa0e4ad72b
0xDC00 <c0d3rR1pp3r@gmail.com>
2023-02-06 00:04:28 +0700

    Add CLOCK ZERO ~Shuuen no Ichibyou~ Devote (Switch, 01008C100C572000)
    Thanks Koukdw#4253

---
+++ NS_01008C100C572000_CLOCK_ZERO_Shuuen_no_Ichibyou_Devote.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         [01008C100C572000] CLOCK ZERO ~Shuuen no Ichibyou~ Devote
+// @version      0.1 - 1.0.0
+// @author       Koukdw
+// @description  Yuzu
+// * Otomate
+// * Idea Factory (アイディアファクトリー)
+// * 
+// ==/UserScript==
+const gameVer = '1.0.0';
+
+const { setHook } = require('./libYuzu.js');
+const mainHandler = trans.send(handler, '200+'); // join 200ms
+
+setHook({
+    '1.0.0': {
+        0x803c290: mainHandler.bind_(null, 0, "name"), // slow but in order
+        0x803c184: mainHandler.bind_(null, 0, "dialogue"), // slow but in order
+        0x801f6d0: mainHandler.bind_(null, 0, "prompt"),
+    }
+}[globalThis.gameVer ?? gameVer]);
+
+function handler(regs, index, hookname) {
+    console.log('onEnter: ', hookname);
+
+    const address = regs[index].value; 
+    //console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
+
+    /* processString */
+    /** @type {string} */
+    let s = address.readShiftJisString();
+    // print rubi
+    const rubis = s.matchAll(/(#Ruby\[)([^,]+).([^\]]+)./g);
+    for (const rubi of rubis) {
+        console.log('rubi', rubi[3]);
+        console.log('rube', rubi[2]);
+    }
+    // remove rubi
+    s = s.replace(/(#Ruby\[)([^,]+).([^\]]+)./g, '$2');
+    
+    s = s.replace(/(#n)+/g, ' ') // Single line
+        .replace(/(#[A-Za-z]+\[(\d*[.])?\d+\])+/g, ''); // Remove controls
+    // #Ruby[森恒犀鳥,もりつねさいちょう]
+    // #n
+    return s;
+}
+
