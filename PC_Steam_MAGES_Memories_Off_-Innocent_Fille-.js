42da0001c1e7de6c4494fe8637d17e8cf66bfa39
0xDC00 <c0d3rR1pp3r@gmail.com>
2023-02-08 19:56:57 +0700

    Add Memories Off: -Innocent Fille- (PC)

---
+++ PC_Steam_MAGES_Memories_Off_-Innocent_Fille-.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Memories Off: -Innocent Fille-
+// @version      0.1
+// @author       [DC]
+// @description  [PC] https://store.steampowered.com/app/738510/Innocent_Fille/
+// * MAGES Engine
+// ==/UserScript==
+require('./PC_Steam_MAGES_Steins;Gate-0.js');
