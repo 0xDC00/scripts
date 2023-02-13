43b7ff5586ca3c65ffb3643a14a53fa29a5faed9
0xDC00 <c0d3rR1pp3r@gmail.com>
2023-02-14 01:28:37 +0700

    Add Persona 3 Portable (Switch, 0100DCD01525A000)

---
+++ NS_0100DCD01525A000_Persona_3_Portable.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         [0100DCD01525A000] Persona 3 Portable
+// @version      0.1 - 1.0.0 (base)
+// @author       Koukdw & [DC]
+// @description  Yuzu
+// * Atlus
+// * 
+// ==/UserScript==
+const gameVer = '1.0.0';
+
+const { setHook } = require('./libYuzu.js');
+const { readString, createTable } = require('./libPCAtlus.js');
+
+const mainHandler = trans.send(handler, '200+'); // join 200ms
+const table = createTable('P3P');
+table[0xa] = ' '; // single line
+
+setHook({
+    '1.0.0': {
+        0x81b9a00: mainHandler
+    }
+}[globalThis.gameVer ?? gameVer]);
+
+function handler(regs) {
+    const type = regs[1].value & 0xFFFF;
+    //console.log(ptr(this.returnAddress) + ' ' + type);
+    if (type === 3) return null;
+
+    const address = regs[0].value;
+    //console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
+    const s = readString(address, table);
+    return s;
+}
