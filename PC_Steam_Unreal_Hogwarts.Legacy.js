f28d8df8fc6e9c6272cf01e03b5519213faf4c7f
0xDC00 <c0d3rR1pp3r@gmail.com>
2023-02-25 16:56:40 +0700

    Add Hogwarts Legacy (PC)

---
+++ PC_Steam_Unreal_Hogwarts.Legacy.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Hogwarts Legacy
+// @version      
+// @author       Koukdw & [DC]
+// @description  Steam
+// * Avalanche Software
+// * Unreal Engine 4
+//
+// https://store.steampowered.com/app/990080/Hogwarts_Legacy/
+// ==/UserScript==
+const UE = require('./libUnrealEngine.js');
+const {
+    getObjectFullName
+} = UE;
+
+const handlerLine = trans.send((s) => s, '250+');
+
+UE.setHookSetText('/Script/UMG.RichTextBlock:SetText', function (thiz, s) {
+    const ctx = getObjectFullName(thiz);
+    console.log('onEnter: ' + ctx);
+
+    if (ctx === '/Engine/Transient.GameEngine.BP_PhoenixGameInstance_C.UI_BP_Subtitle_Element_C.WidgetTree.Text_Element') {
+        s = s.seplace(/^<Name_Text>/, '')
+            .seplace(':</> ', '\r\n')
+            ;
+        handlerLine(s);
+    }
+    else if (ctx === ctx === '/Engine/Transient.GameEngine.BP_PhoenixGameInstance_C.UI_BP_MissionBannerCheckbox_C.WidgetTree.CheckboxText') {
+        handlerLine(s);
+    }
+    else {
+        console.warn(s);
+    }
+});
