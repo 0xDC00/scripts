538850f25ffb6061003001fcf0f58f562ac74f6a
Koukdw <12419106+koukdw@users.noreply.github.com>
2023-03-10 11:47:24 +0100

    improve style

--- PC_Steam_Unity_Soul.Hackers.2.js
+++ PC_Steam_Unity_Soul.Hackers.2.js
@@ -19,62 +19,61 @@ const {
 const handlerLine = trans.send((s) => s, '250+');
 
 // Subtitle
-Mono.setHook("", ".EventSubtitleManager", "Show", -1, {
-    onEnter: (args) => {
-        console.log("onEnter EventSubtitleManager:Show");
+Mono.setHook('', '.EventSubtitleManager', 'Show', -1, {
+    onEnter(args) {
+        console.log('onEnter EventSubtitleManager:Show');
         let text = args[1].readMonoString()
         let s = cleanText2(text);
         handlerLine(s);
     }
-})
+});
 
 // Dialogue + Name
-Mono.setHook("", ".EventTalkController", "SetMessageText", 5, {
-    onEnter: (args) => {
-        console.log("onEnter EventTalkController:SetMessageText");
+Mono.setHook('', '.EventTalkController', 'SetMessageText', 5, {
+    onEnter(args) {
+        console.log('onEnter EventTalkController:SetMessageText');
         let text = args[1].readMonoString();
         let name = args[2].readMonoString();
         text = cleanText(text);
         name = cleanText(name);
-        handlerLine(name + "\n" + text);
+        handlerLine(name + '\r\n' + text);
     }
-})
+});
 
 // Choices
-Mono.setHook("", "Game.UI.Common.UISelectWindow", "SetText", -1, {
-    onEnter: function(args){
-        console.log("onEnter Game.UI.Common.UISelectWindow:SetText");
+Mono.setHook('', 'Game.UI.Common.UISelectWindow', 'SetText', -1, {
+    onEnter(args) {
+        console.log('onEnter Game.UI.Common.UISelectWindow:SetText');
         let text = args[2].readMonoString();
         let s = cleanText(text);
         handlerLine(s);
     }
-})
+});
 
 // Tips
-Mono.setHook("", "Game.Common.TipsManager", "GetNameMessage", -1, {
+Mono.setHook('', 'Game.Common.TipsManager', 'GetNameMessage', -1, {
     onLeave(retValue) {
-        console.log("onLeave Game.Common.TipsManager:GetNameMessage");
+        console.log('onLeave Game.Common.TipsManager:GetNameMessage');
         let text = retValue.readMonoString();
         let s = cleanText(text);
         handlerLine(s);
     }
-})
+});
 
-Mono.setHook("", "Game.Common.TipsManager", "GetTextMessage", -1, {
+Mono.setHook('', 'Game.Common.TipsManager', 'GetTextMessage', -1, {
     onLeave(retValue) {
-        console.log("onLeave Game.Common.TipsManager:GetTextMessage");
+        console.log('onLeave Game.Common.TipsManager:GetTextMessage');
         let text = retValue.readMonoString();
         let s = cleanText2(text);
         handlerLine(s);
     }
-})
-
+});
 
 // Tutorial
 {
     const target = Mono.use('AtLib', 'AtLib.AtText').SetText.address;
     const targetStr = target.toString();
-    console.log(`target = ${targetStr}`);
+    console.log('Target = ' + targetStr);
     let setTextAddr = [];
 
     // find call
@@ -90,70 +89,71 @@ Mono.setHook("", "Game.Common.TipsManager", "GetTextMessage", -1, {
         }
         ins = Instruction.parse(ins.next);
     }
+
     setTextAddr.forEach((ins, index) => {
         if (index === 0 || index === 2) {
-            console.log(`Hooking SetText at ${ins.address.sub(_module.base).add(0x180000000)}`);
-            Breakpoint.add(ins.address, function() {
-                console.log(`onEnter: SetText at ${ins.address.sub(_module.base).add(0x180000000)}`);
+            const idaAddress = ins.address.sub(_module.base).add(0x180000000).toString();
+            console.log('Hooking SetText at ' + idaAddress);
+
+            Breakpoint.add(ins.address, function () {
+                console.log('onEnter: SetText at ' + idaAddress);
                 let text = this.context.rdx.readMonoString();
                 let s = cleanText2(text);
                 handlerLine(s);
-            })
-            // Interceptor makes the game crash
+            });
 
+            //// Interceptor makes the game crash
             // Interceptor.attach(ins.address, {
             //     onEnter: function (args) {
-            //         console.log(`onEnter: SetText at ${ins.address.sub(_module.base).add(0x180000000)}`);
+            //         console.log('onEnter: SetText at ' + idaAddress);
             //         let text = args[1].readMonoString();
             //         let s = cleanText2(text);
             //         handlerLine(s);
             //     }
-            // })
+            // });
         }
     });
 }
 
-
-// Prompt text (Annoying)
-// Mono.setHook("", "Game.UI.Common.UICommonDialogBox", "SetText", -1, {
-//     onEnter: function(args) {
-//         console.log("onEnter Game.UI.Common.UICommonDialogBox:SetText");
+//// Prompt text (Annoying)
+// Mono.setHook('', 'Game.UI.Common.UICommonDialogBox', 'SetText', -1, {
+//     onEnter(args) {
+//         console.log('onEnter Game.UI.Common.UICommonDialogBox:SetText');
 //         let text = args[1].readMonoString();
 //         handlerLine(text);
-//     } 
-// })
+//     }
+// });
 
-// Prompt choice (Annoying)
-// Mono.setHook("", "Game.UI.Common.UICommonDialogBoxSelector", "SetText", -1, {
-//     onEnter: function(args){
-//         console.warn("onEnter Game.UI.Common.UICommonDialogBoxSelector:SetText");
+//// Prompt choice (Annoying)
+// Mono.setHook('', 'Game.UI.Common.UICommonDialogBoxSelector', 'SetText', -1, {
+//     onEnter(args) {
+//         console.warn('onEnter Game.UI.Common.UICommonDialogBoxSelector:SetText');
 //         let text = args[1].readMonoString();
 //         let s = text;
 //         handlerLine(s);
 //     }
-// })
+// });
 
 
-// Notification + spell names (Annoying) uncomment if you really want this
-// Mono.setHook("", "Game.UI.Battle.BattleActionPanel", "SetText", -1, {
-//     onEnter: function(args){
-//         console.warn("onEnter Game.UI.Battle.BattleActionPanel:SetText", this.returnAddress.sub(_module.base).add(0x180000000));
+//// Notification + spell names (Annoying) uncomment if you really want this
+// Mono.setHook('', 'Game.UI.Battle.BattleActionPanel', 'SetText', -1, {
+//     onEnter(args) {
+//         console.warn('onEnter Game.UI.Battle.BattleActionPanel:SetText', this.returnAddress.sub(_module.base).add(0x180000000));
 //         let text = args[1].readMonoString();
 //         let s = text;
 //         handlerLine(s);
 //     }
-// })
+// });
 
-
-// Root hook (get everything)
-// Mono.setHook("AtLib", "AtLib.AtText", "SetText", -1, {
-//     onEnter: function(args){
-//         console.warn("onEnter AtLib.AtText:SetText", this.returnAddress.sub(_module.base).add(0x180000000));
+//// Root hook (get everything)
+// Mono.setHook('AtLib', 'AtLib.AtText', 'SetText', -1, {
+//     onEnter(args) {
+//         console.warn('onEnter AtLib.AtText:SetText', this.returnAddress.sub(_module.base).add(0x180000000));
 //         let text = args[1].readMonoString();
 //         let s = text;
 //         console.warn(s);
 //     }
-// })
+// });
 
 function cleanText(s) {
     return s
