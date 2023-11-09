72181c99db3ac98cf0d365bca4b2e3667fa2e83c
aqui <42970445+aquiab@users.noreply.github.com>
2023-11-08 23:06:49 -0300

    Update PC_Steam_Ni.No.Kuni.Wrath.Of.The.White.Witch.Remastered.js

--- PC_Steam_Ni.No.Kuni.Wrath.Of.The.White.Witch.Remastered.js
+++ PC_Steam_Ni.No.Kuni.Wrath.Of.The.White.Witch.Remastered.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Ni no Kuni Wrath of the White Witch™ Remastered
-// @version      0.1
+// @version      0.2
 // @author       aqui
 // @description  Steam
 // * Level-5
@@ -12,13 +12,13 @@ const handlerLine = trans.send((s) => s, '250+');
 
 // dialogues and descriptions
 (function () {
-    const dialogSig1 = 'E8 2EBDF2FF'; // E8 5CADB2FF just dialogues
+    const dialogSig1 = '0F?? ?? ?? ???????? 49 ?? ?? 46 ?? ?? ?? 75 ?? 48 ?? ?? ?? E8'; //E8 2EBDF2FF
     const results = Memory.scanSync(__e.base, __e.size, dialogSig1);
     if (results.length === 0) {
         console.error('[DialoguesPattern] no result!');
         return;
     }
-    let hookAddress = results[0].address
+    let hookAddress = results[0].address.add(0x15)
     console.log('[DialoguesPattern] ' + hookAddress);
 
     let lastTwentyStrings = []
