a8a49bebdcfce7607f0d3bb7d52301d34c40c8c9
0xDC00 <c0d3rR1pp3r@gmail.com>
2023-12-22 03:13:58 +0700

    Add Angelique Retour (PSVita, PCSG00696; thanks zooo)

---
+++ PSVita_PCSG00696_Angelique_Retour.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         [PCSG00696] Angelique Retour
+// @version      0.1
+// @author       [zooo]
+// @description  Vita3k
+// ==/UserScript==
+trans.replace(function (s) {
+    return s
+        .replace(/㌔/g, '⁉')
+        ;
+});
+//------------------------------------------------
+const { setHook } = require("./libVita3k.js");
+
+const mainHandler = trans.send(handler, "200+"); // join 200ms
+
+setHook({
+    0x8008bd1a: mainHandler.bind_(null, 1, 0, "text1"),
+    0x8008cd48: mainHandler.bind_(null, 0, 0, "text2"),
+    0x8008f75a: mainHandler.bind_(null, 0, 0, "choice"),
+});
+
+function handler(regs, index, offset, hookname) {
+    const address = regs[index].value.add(offset);
+
+    console.log("onEnter: " + hookname);
+    console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
+
+    let s = address.readShiftJisString();
+
+
+    return s;
+}
