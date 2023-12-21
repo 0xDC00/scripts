da51abd41a908913d0cd0e61c64096071fa3c434
0xDC00 <c0d3rR1pp3r@gmail.com>
2023-12-22 03:09:14 +0700

    Add BLACK WOLVES SAGA  -Weiβ und Schwarz- (PSVita, PCSG00935; thanks Koukdw)

---
+++ PSVita_PCSG00935_BLACK_WOLVES_SAGA_Weiβ_und_Schwarz.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         [PCSG00935] BLACK WOLVES SAGA  -Weiβ und Schwarz-
+// @version      0.1
+// @author       Koukdw
+// @description  Vita3k
+// * Otomate / Rejet
+// * Idea Factory Co., Ltd.
+// ==/UserScript==
+const { setHook } = require("./libVita3k.js");
+
+const mainHandler = trans.send(handler, "200+"); // join 200ms
+
+setHook({
+    0x8004ed22: mainHandler.bind_(null, 0, 0, "name"),
+    0x8006d202: mainHandler.bind_(null, 1, 2, "text"),
+});
+
+function handler(regs, index, offset, hookname) {
+    const address = regs[index].value.add(offset);
+
+    console.log("onEnter", hookname);
+    console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
+
+    let s = address.readUtf8String();
+
+    const rubis = s.matchAll(/(#Ruby\[)([^,]+).([^\]]+)./g);
+    for (const rubi of rubis) {
+        console.log("rubi", rubi[3]);
+        console.log("rube", rubi[2]);
+    }
+    // remove rubi
+    s = s.replace(/(#Ruby\[)([^,]+).([^\]]+)./g, "$2");
+
+    s = s
+        .replace(/(#n)+/g, " ") // Single line
+        .replace(/(#[A-Za-z]+\[(\d*[.])?\d+\])+/g, ""); // Remove controls
+
+    return s;
+}
