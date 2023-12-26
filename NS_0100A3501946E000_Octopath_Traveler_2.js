952438b4c8743dfdfd732ac8248d238146559973
Kalleo <124850354+UMNV@users.noreply.github.com>
2023-12-26 15:36:45 -0300

    Add Octopath traveler 2

---
+++ NS_0100A3501946E000_Octopath_Traveler_2.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         [0100A3501946E000] Octopath Traveler II
+// @version      1.0.0
+// @author       [Kalleo]
+// @description  Yuzu
+// * Square Enix
+// *
+// ==/UserScript==
+const gameVer = '1.0.0';
+
+const { setHook } = require('./libYuzu.js');
+const mainHandler = trans.send(handler, '200+');
+
+setHook({
+    '1.0.0': {
+        [0x8088a4d4 - 0x80004000]: mainHandler.bind_(null, 0, "main text"),
+    }
+}[globalThis.gameVer = globalThis.gameVer ?? gameVer]);
+
+
+function handler(regs, index, hookname) {
+    const address = regs[index].value;
+    //console.log('onEnter', hookname);
+
+    /* processString */
+    //console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
+    let s = address.readUtf16String();
+
+    return s;
+}
