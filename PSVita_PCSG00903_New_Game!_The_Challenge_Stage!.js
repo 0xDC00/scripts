2c45561036b25de03d69440bd84190d073f08b74
0xDC00 <c0d3rR1pp3r@gmail.com>
2023-12-28 00:11:17 +0700

    New Game! The Challenge Stage!: address for latest Vita3k

--- PSVita_PCSG00903_New_Game!_The_Challenge_Stage!.js
+++ PSVita_PCSG00903_New_Game!_The_Challenge_Stage!.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         [PCSG00903] New Game! The Challenge Stage!
-// @version      0.1
+// @version      1.00
 // @author       [DC]
 // @description  Vita3k
 // * MAGES. GAME
@@ -9,14 +9,14 @@
 const { setHook } = require("./libVita3k.js");
 
 setHook({
-    // 0x0022b738(2) 0x0022b74c(0)
-    0x0022b74c: trans.send(handler.bind_(null, 0), '200+') // join 200ms (dialouge + choice + ...; \n); x0
+    // 0x0022b738(2) 0x0022b74c(0) | 0x0022b74c-0x00109000+0x80004000=0x8012674c
+    0x8012674c: trans.send(handler.bind_(null, 0), '200+') // join 200ms (dialouge + choice + ...; \n); x0
 });
 
 function handler(regs, index) {
     const address = regs[index].value;
 
-    console.log('onEnter', this.context.pc);
+    console.warn('onEnter');
     //console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
     
     /* processString */
