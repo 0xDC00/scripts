74be707c0f66ee61974192704047b9bb17a3d58c
Kalleo <124850354+UMNV@users.noreply.github.com>
2024-01-02 19:06:49 -0300

    Fix hooks

--- NS_01008C0016544000_Sea_of_Stars.js
+++ NS_01008C0016544000_Sea_of_Stars.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         [01008C0016544000] Sea of Stars
-// @version      1.0.47140 (If you can't find this version, remove all updates and hook the base game with 0x83e93ca0)
+// @version      1.0.45861 - 1.0.47140
 // @author       [Kalleo]
 // @description  Yuzu
 // * Sabotage Studio
@@ -12,8 +12,11 @@ const { setHook } = require('./libYuzu.js');
 const mainHandler = trans.send(handler, '200+');
 
 setHook({
+    '1.0.45861': {
+        [0x83e93ca0 - 0x80004000]: mainHandler.bind_(null, 0, "Main text"),
+    },
     '1.0.47140': {
-        [0x820c3fa0 - 0x80004000]: mainHandler.bind_(null, 0, "Main text"), // Use 0x83e93ca0 for the base game (1.0.45861)
+        [0x820c3fa0 - 0x80004000]: mainHandler.bind_(null, 0, "Main text"),
     }
 }[globalThis.gameVer = globalThis.gameVer ?? gameVer]);
 
