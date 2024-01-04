ad613b58d53817c297ebfcb344c624801e1dbde4
zooo <90264848+iiuuoow@users.noreply.github.com>
2024-01-05 00:29:59 +0900

    Update _MY Cupid parasite.js

--- _MY Cupid parasite.js
+++ _MY Cupid parasite.js
@@ -7,42 +7,35 @@
 // ==/UserScript==
 
 const gameVer = '1.0.1';
-
+const decoder = new TextDecoder('utf-32');
 const { setHook } = require('./libYuzu.js');
-const mainHandler = trans.send(handler, '200+');
+
+const mainHandler = trans.send(handler, '200+'); // join 200ms
 
 setHook({
     '1.0.1': {
-        [0x80169df0 - 0x80004000]: mainHandler, // choice
+        [0x80057910 - 0x80004000]: mainHandler.bind_(null, 2, "name + text"), 
+        [0x80169df0 - 0x80004000]: mainHandler.bind_(null, 0, "choice"),
     }
+
 }[globalThis.gameVer = globalThis.gameVer ?? gameVer]);
 
-function handler(regs) {
-    const address = regs[0].value;
+function handler(regs, index, hookname) {
+    const reg = regs[index];
     console.log('onEnter');
+
+
+    console.log('onEnter: ' + hookname);
+    const address = reg.value;
+    //console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
 
     /* processString */
     let s = address.readUtf32StringLE()
-        .replaceAll('\n', ' ') // Single line
+    s = s.replaceAll(/[\s]/g,'');
+    s = s.replaceAll('#KW','')
+    s = s.replaceAll('#C(TR,0xff0000ff)','');
+
         ;
     return s;
+
 }
-
-globalThis._FIXED_DCODE_ = '$utf-32,|100|00000000,458B6405004589E44D89A7B8000000498B87A80000004531E44C01E04989C349C1EB270F85C70000004D8B6405004D896750488B44243041BB1C000000'; // dialogue
-
-// globalThis.decode = function(buffer) {
-//  return _decode(buffer);
-// }
-
-
-globalThis.filters = function filters(s) {
-  s = _filters(s);
-  s=s.replaceAll('#C(TR,0xff0000ff)','');
-  s=s.replaceAll('#KW','')
-  s=s.replaceAll(/[\s]/g,'');
-    
-    ;
-  return s;
-}
-
-require('_ExecutionWatch.js');
