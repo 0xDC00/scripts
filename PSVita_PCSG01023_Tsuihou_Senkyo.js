456622b6a298e5f976f6a212cf54fb9116daf7bc
amy10111111111 <102473286+amy10111111111@users.noreply.github.com>
2024-02-01 17:23:57 +0200

    Adding new PSVita scripts
    Add Tsuihou Senkyo
    Add Sanzen Sekai Yuugi ~MultiUniverse Myself~

---
+++ PSVita_PCSG01023_Tsuihou_Senkyo.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         [PCSG01023]  Tsuihou Senkyo
+// @version      0.1
+// @author       GO123
+// @description  Vita3k
+// *Matatabi
+// ==/UserScript==
+trans.replace(function (s) {
+    return s
+        .replace(/<br>/g, '')
+        .replace(/%CF11F/g, "")
+        .replace(/%CFFFF/g, "")
+        .replace(/%K%P/g, '')
+        .replace(/%K%N/g, '')
+        .replaceAll("\n", '')
+
+        ;
+});
+//------------------------------------------------
+const { setHook } = require("./libVita3k.js");
+
+const mainHandler = trans.send(handler, "200+"); // join 200ms
+
+setHook({
+    0x8002e176: mainHandler.bind_(null, 0, 0, "dialogue"),//dialogue+name
+});
+
+function handler(regs, index, offset, hookname) {
+    const address = regs[index].value.add(offset);
+
+    console.log("onEnter: " + hookname);
+    console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
+
+    let s = address.readShiftJisString();
+
+
+    return s;
+}
