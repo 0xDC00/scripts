e53fbfb35d15e81ff159d3d97e93acb7a1d4e96e
Koukdw <12419106+koukdw@users.noreply.github.com>
2024-02-06 17:08:49 +0100

    Fix formatting

--- PSVita_PCSG00706_Moshi_Kono_Sekai_ni_Kami_sama_ga_Iru_to_suru_Naraba.js
+++ PSVita_PCSG00706_Moshi_Kono_Sekai_ni_Kami_sama_ga_Iru_to_suru_Naraba.js
@@ -18,8 +18,8 @@ const mainHandler = trans.send(handler, "200+"); // join 200ms
 
 //however when the game goes NVL it will extract the whole box instead of line by line 
 setHook({
-    0x80c1f270: mainHandler.bind_(null, 0, 0, "dialogue"),//dialogue+ textmessage 
-    0x80d48bfc: mainHandler.bind_(null, 1, 0, "Dictionary1"),//Dictionary1
+    	0x80c1f270: mainHandler.bind_(null, 0, 0, "dialogue"),//dialogue+ textmessage 
+    	0x80d48bfc: mainHandler.bind_(null, 1, 0, "Dictionary1"),//Dictionary1
 	0x80d48c20: mainHandler.bind_(null, 0, 0, "Dictionary2"),//Dictionary2
 });
 
@@ -27,7 +27,7 @@ function handler(regs, index, offset, hookname) {
     const address = regs[index].value.add(offset);
 
     console.log("onEnter: " + hookname);
-    console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
+    //console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
 
     const len = address.add(0x8).readU32() * 2;
     let s = address.add(0xC).readUtf16String(len);
