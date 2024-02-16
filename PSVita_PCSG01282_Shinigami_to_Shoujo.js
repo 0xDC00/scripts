fe1bdeb782149d3c4366c2cb7267251cf903a70b
amy10111111111 <102473286+amy10111111111@users.noreply.github.com>
2024-02-16 18:18:40 +0200

    Update PSVita_PCSG01282_Shinigami_to_Shoujo.js
    Rewrite script to handle text better

--- PSVita_PCSG01282_Shinigami_to_Shoujo.js
+++ PSVita_PCSG01282_Shinigami_to_Shoujo.js
@@ -10,27 +10,30 @@
 const { setHook } = require("./libVita3k.js");
 
 const mainHandler = trans.send(handler, "200+"); // join 200ms
-
+const mainHandler1 = trans.send(handler, "200++");
 setHook({
-   
+
   0x800204ba: mainHandler.bind_(null, 2, 0, "dialogueNVL"),
-  0x8000f00e: mainHandler.bind_(null, 1, 0, "dialogue main"),
-  0x80011f1a: mainHandler.bind_(null, 0, 0, "Name"),
+  0x8000f00e: mainHandler1.bind_(null, 1, 0, "dialogue main"),
+  0x80011f1a: mainHandler1.bind_(null, 0, 0, "Name"),
   0x8001ebac: mainHandler.bind_(null, 1, 0, "choices"),
 });
-
+let previous = "";
 function handler(regs, index, offset, hookname) {
-    const address = regs[index].value.add(offset);
+  const address = regs[index].value.add(offset);
 
-   // console.log("onEnter: " + hookname);
-   // console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
+  // console.log("onEnter: " + hookname);
+  // console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
 
-    let s = address.readShiftJisString()
-      .replaceAll(/[\s]/g,'')
-      .replaceAll(/\\n/g,'')
-      .replaceAll(/\\d/g,'')
-      .replace(/@[a-z]/g, "")
-	 ; 
-
-    return s;
+  let s = address.readShiftJisString()
+    .replaceAll(/[\s]/g, '')
+    .replaceAll(/\\n/g, '')
+    .replaceAll(/\\d/g, '')
+    .replace(/@[a-z]/g, "")
+    ;
+  if (s === previous) {
+    return;
+  }
+  previous = s;
+  return s;
 }
