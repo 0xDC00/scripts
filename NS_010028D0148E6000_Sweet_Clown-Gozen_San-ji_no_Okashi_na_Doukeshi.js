d1cef85c328244104d6c9e6f2c8b5045a1673d58
StephaniePoll <spoll808@gmail.com>
2024-02-18 13:53:51 -0500

    start trying to hook choice dialog, in prog

--- NS_010028D0148E6000_Sweet_Clown-Gozen_San-ji_no_Okashi_na_Doukeshi.js
+++ NS_010028D0148E6000_Sweet_Clown-Gozen_San-ji_no_Okashi_na_Doukeshi.js
@@ -14,21 +14,33 @@ globalThis.ARM = true;
 // Issue is unrelated to this script, text should still be correct
 const { setHook } = require('./libYuzu.js');
 
-const mainHandler = trans.send(handler, '250+');
+const mainHandler = trans.send(handler, '200+');
+// const choiceHandler = trans.send(handler.bind_(null, 3, 'choice'), '250+');
 
 setHook({
     '1.0.2': {
-        0x20dbfc: mainHandler
+        0x20dbfc: mainHandler.bind_(null, 0, "dialog"),
+        // 0x214978: choiceHandler ,// called 30 something times but contains all choices
+        // 0x214930: choiceHandler, // works first call only, after that is access violation accessing 0x26853c73f04
     }
 }[globalThis.gameVer = globalThis.gameVer ?? gameVer]);
 
-function handler(regs) {
-    const address = regs[0].value;
+function handler(regs, index, hookname) {
+    const address = regs[index].value;
+    // console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
 
-    let s = address.add(0x28).readShiftJisString();
-     s = s
+    let s;
+    if (hookname === 'dialog') {
+        s = address.add(0x28).readShiftJisString();
+    } else {
+        // grab strings for 0x214978, commented out currently
+        // s = address.add(0xC).readShiftJisString();
+    }
+
+    s = s
         .replace(/{|\/.*?}|\[.*?\]/g, '')
         .replace(/(\\c|\\n)+/g, ' ')
+        // .replace(/,.*$/,' ') // clean strings for 0x214978, commented out currently
     ;
 
     return s;
