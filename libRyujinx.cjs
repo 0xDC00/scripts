93e6951f7ea4c397bdde2fd34aeb398fb5daf34d
0xDC00 <c0d3rR1pp3r@gmail.com>
2024-04-28 01:40:36 +0700

    Add libRyujinx.cjs

---
+++ libRyujinx.cjs
@@ -0,0 +1,7 @@
+console.warn('[Compatibility]');
+console.warn('Ryujinx 1.1.1282++');
+console.log('[Mirror] Download: https://github.com/koukdw/emulators/releases');
+
+module.exports = exports = [
+    'H458d5136f', 0, 0
+];
