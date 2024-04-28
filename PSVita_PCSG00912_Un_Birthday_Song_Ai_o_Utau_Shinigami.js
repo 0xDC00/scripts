12db18b6950248008f9b0a632458a20c9c4d544a
amy10111111111 <102473286+amy10111111111@users.noreply.github.com>
2024-04-29 01:16:02 +0300

    Adding scripts for the Birthday Song Series (PSvita)
    Re:Birthday Song ~Koi o Utau Shinigami~
    Un:Birthday Song ~Ai o Utau Shinigami~

---
+++ PSVita_PCSG00912_Un_Birthday_Song_Ai_o_Utau_Shinigami.js
@@ -0,0 +1,76 @@
+// ==UserScript==
+// @name         [PCSG00912]  Un:Birthday Song ~Ai o Utau Shinigami~
+// @version      0.1
+// @author       GO123 (huge thanks to koukdw )
+// @description  Vita3k
+// *honeybee
+// ==/UserScript==
+/*trans.replace(function (s) {
+    return s
+        .replaceAll(/%N/g, '')
+        ;
+});*/
+//------------------------------------------------
+const { setHook } = require("./libVita3k.js");
+
+
+const nvlHandler = trans.send(nvl_handler, -200); // join 200ms
+const mainHandler = trans.send(handler, '200++');
+
+setHook({
+
+    // 0x800379ae: nvlHandler.bind_(null, 3, 0x0, "dialogue NVL"),
+    0x80038538: nvlHandler.bind_(null, 1, 0x0, "dialogue NVL"),
+    0x80004b52: mainHandler.bind_(null, 3, 0x5, "dialogue"),
+
+
+});
+
+function nvl_handler(regs, index, offset, hookname) {
+    let address = regs[index].value;
+    const encoder = new TextEncoder("shift_jis");
+    let final_string = ""
+    const pattern = '47 ff ff'
+    const results = Memory.scanSync(address, 0x50, pattern);
+    if (results.length === 0) {
+        return;
+    }
+    address = results[0].address.add(5);
+
+    while (true) {
+
+        let text = address.readShiftJisString();
+        final_string += text;
+        address = address.add(encoder.encode(text).byteLength + 1);
+
+
+
+        let bytes = [address.readU8(), address.add(1).readU8(), address.add(2).readU8()];
+        console.log(hexdump(address, { header: false, ansi: false }))
+        if (!(bytes[0] == 0x48 && bytes[1] == 0xFF && bytes[2] == 0xFF)) break;
+        address = address.add(3);
+        bytes = [address.readU8(), address.add(1).readU8(), address.add(2).readU8()];
+        if (!(bytes[0] == 0x47 && bytes[1] == 0xFF && bytes[2] == 0xFF)) break;
+
+        address = address.add(5);
+
+    }
+
+    return final_string;
+
+
+}
+
+function handler(regs, index, offset, hookname) {
+    let address = regs[index].value;
+    let s = address.add(offset).readShiftJisString();
+    return s
+        .replace(/G��./g, "")
+        .replace(/G��/g, "")
+        .replaceAll(/%N/g, '')
+
+
+        ;
+
+
+}
