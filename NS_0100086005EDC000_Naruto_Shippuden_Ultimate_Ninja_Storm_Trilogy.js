605985aef99173ff317f3f0eaf9d804a67a9541f
0xDC00 <c0d3rR1pp3r@gmail.com>
2024-05-03 10:05:31 +0700

    Format NS_0100086005EDC000_Naruto_Shippuden_Ultimate_Ninja_Storm_Trilogy.js

--- NS_0100086005EDC000_Naruto_Shippuden_Ultimate_Ninja_Storm_Trilogy.js
+++ NS_0100086005EDC000_Naruto_Shippuden_Ultimate_Ninja_Storm_Trilogy.js
@@ -21,7 +21,7 @@ setHook({
         "Hf1914b62b3f617d1": mainHandler.bind_(null, 1, "Menu ST1"),
         "Heab9cf18a83c9552": mainHandler.bind_(null, 0, "Tutorial Description ST1"),
         "Hdddc8e939d7e2d45": mainHandler.bind_(null, 1, "Mission Description ST1"),
-        
+
         //Storm 2
         "H341a877be3696c23": mainHandler.bind_(null, 1, "Cutscene ST2"),
         "H9b5df99cc24584b0": mainHandler.bind_(null, 1, "Name ST2"),
@@ -33,14 +33,13 @@ setHook({
         "Hd84d13ccc2c365d1": mainHandler.bind_(null, 0, "Info ST2"),
         "H33c142f4fb00e2bf": mainHandler.bind_(null, 0, "Menu ST2"),
         "H986d9498d333f4f6": mainHandler2.bind_(null, 0, "Objective ST2"),
-        
+
         // Storm 3
         "He0a0a5b7c955cb75": mainHandler.bind_(null, 1, "All Text ST3"),
-}
+    }
 }, globalThis.gameVer = globalThis.gameVer ?? gameVer);
 
 let previous = "";
-
 function handler(regs, index, hookname) {
     const address = regs[index].value;
     // console.log('onEnter: ' + hookname);
@@ -50,12 +49,12 @@ function handler(regs, index, hookname) {
     let s = address.readUtf8String();
 
     s = s
-    .replace(/<ruby([^|>]+)\|[^>]+>/g, '$1')
-    .replace(/<([^>]+)ruby([^>]+)>/g, '$1$2')
-    .replace(/<[^>]*>/g, '')
+        .replace(/<ruby([^|>]+)\|[^>]+>/g, '$1')
+        .replace(/<([^>]+)ruby([^>]+)>/g, '$1$2')
+        .replace(/<[^>]*>/g, '')
 
     if (s === previous) {
-        return;
+        return null;
     }
     previous = s;
 
