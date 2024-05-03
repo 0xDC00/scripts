2bc27e3c2de5fb2db81286be84a9225c364081a4
クロシオ <61211787+blacktide082@users.noreply.github.com>
2024-05-02 22:54:19 -0400

    Add choices hook for Utakata no Uchronia (#446)

--- NS_010027401A2A2000_Utakata_no_Uchronia.js
+++ NS_010027401A2A2000_Utakata_no_Uchronia.js
@@ -13,18 +13,19 @@ const mainHandler = trans.send(handler);
 
 setHook({
     '1.0.0': {
-        [0x8180de40 - 0x80004000]: mainHandler, // text box
-        [0x816b61c0 - 0x80004000]: mainHandler, // dictionary
+        [0x8180de40 - 0x80004000]: mainHandler.bind_(null, 0, "text"),
+        [0x816b61c0 - 0x80004000]: mainHandler.bind_(null, 0, "dictionary"),
+        [0x815fe594 - 0x80004000]: mainHandler.bind_(null, 0, "choices"),
     }
 }[globalThis.gameVer = globalThis.gameVer ?? gameVer]);
 
-function handler(regs) {
-    const address = regs[0].value;
+function handler(regs, index, name) {
+    const address = regs[index].value;
     const len = address.add(0x10).readU16() * 2;
     let text = address.add(0x14).readUtf16String(len);
     text = text.replace(/\[dic.*?text=/g, ''); // dictionary words
     text = text.replace(/\[|'.*?\]/g, ''); // ruby text
     text = text.replace(/\]/g, ''); // closing brace if no ruby text
-    text = text.replace(/\s|　/g, ''); // remove whitespace
+    text = text.replace(name === 'choices' ? /[^\S\n]|　/g : /\s|　/g, ''); // remove whitespace (leave line breaks for choices)
     return text;
 }
