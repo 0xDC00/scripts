9c3bd755a16cc787ae5871e6f4282ebcf22bdf68
0xDC00 <c0d3rR1pp3r@gmail.com>
2024-05-03 09:56:07 +0700

    Update NS_0100A77018EA0000_Dragon_Quest_Monsters_The_Dark_Prince.js
    format

--- NS_0100A77018EA0000_Dragon_Quest_Monsters_The_Dark_Prince.js
+++ NS_0100A77018EA0000_Dragon_Quest_Monsters_The_Dark_Prince.js
@@ -13,9 +13,9 @@ const mainHandler = trans.send(handler, '200+');
 
 setHook({
     '1.0.6': {
-        "Hb4462d78e1881de9": mainHandler.bind_(null, 1, "Text"), 
+        "Hb4462d78e1881de9": mainHandler.bind_(null, 1, "Text"),
         "Hbd4bb92daec3d3b2": mainHandler.bind_(null, 0, "Config Description"),
-}
+    }
 }, globalThis.gameVer = globalThis.gameVer ?? gameVer);
 
 function handler(regs, index, hookname) {
@@ -28,8 +28,8 @@ function handler(regs, index, hookname) {
     let s = address.add(0x14).readUtf16String(len);
 
     s = s
-    .replace(/<space=-1\.00em><voffset=1em><size=50%>.*?<\/size><\/voffset><space=0\.12em>/g, "") // Remove Furigana
-    .replace(/<[^>]*>/g, '') // Remove HTML Tags
+        .replace(/<space=-1\.00em><voffset=1em><size=50%>.*?<\/size><\/voffset><space=0\.12em>/g, "") // Remove Furigana
+        .replace(/<[^>]*>/g, '') // Remove HTML Tags
 
     return s;
 }
