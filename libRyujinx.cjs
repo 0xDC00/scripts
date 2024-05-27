c8e7f3d1db30255675e734b3a87be32e0c860727
0xDC00 <c0d3rR1pp3r@gmail.com>
2024-05-27 20:32:12 +0700

    libRyujinx: support latest Ryujinx 1.1.1325, linux support

--- libRyujinx.cjs
+++ libRyujinx.cjs
@@ -1,7 +1,13 @@
 console.warn('[Compatibility]');
-console.warn('Ryujinx 1.1.1282++');
+console.warn('Ryujinx 1.1.1325++');
 console.log('[Mirror] Download: https://github.com/koukdw/emulators/releases');
 
+console.log(`
+1. ReLaunch Ryujinx (dont boot the game yet).
+2. Attach.
+3. Boot game.
+`);
+
 module.exports = exports = [
-    'H458d5136f', 0, 0
+    'H458d5127f', 'H45a45971f', 0
 ];
