ccedb8c660d35268338afc5f84b28573ae5a0d21
Mansive <xmansive@gmail.com>
2024-10-14 05:02:39 -0500

    Fix Natsuzora no Monologue for Ryujinx

--- NS_01000E701DAE8000_Natsuzora_no_Monologue.js
+++ NS_01000E701DAE8000_Natsuzora_no_Monologue.js
@@ -2,35 +2,34 @@
 // @name         [01000E701DAE8000] Natsuzora no Monologue ~Another Memory~
 // @version      1.0.0
 // @author       kenzy
-// @description  yuzu/sudachi
+// @description  Yuzu/Sudachi, Ryujinx
 // * Design Factory & Otomate
 // * Idea Factory
 // ==/UserScript==
-const gameVer = '1.0.0';
+const gameVer = "1.0.0";
 
-globalThis.ARM = true;
-const { setHook } = require('./libYuzu.js');
-const mainHandler = trans.send(handler, '200+');
+const { setHook } = require("./libYuzu.js");
+const mainHandler = trans.send(handler, "200+");
 
-setHook({
-    '1.0.0': {
-        [0x8006007c - 0x204000]: mainHandler.bind_(null, 0, "dialogue"),  // dialogue & names
-        [0x800578c4 - 0x204000]: mainHandler.bind_(null, 2, "choices")
-    }
-
-}[globalThis.gameVer = globalThis.gameVer ?? gameVer]);
+setHook(
+  {
+    "1.0.0": {
+      [0x8006007c - 0x80004000]: mainHandler.bind_(null, 0, "dialogue"), // dialogue & names
+      [0x800578c4 - 0x80004000]: mainHandler.bind_(null, 1, "choices"),
+    },
+  }[(globalThis.gameVer = globalThis.gameVer ?? gameVer)]
+);
 
 function handler(regs, index, hookname) {
-    const reg = regs[index];
-    const address = reg.value;    
+  const reg = regs[index];
+  const address = reg.value;
 
-    /* processString */
-    let s = address.readShiftJisString();  
-    s = s
-    .replace(/(#n)+/g, ' ') 
-    .replace(/(#[A-Za-z]+\[(\d*[.])?\d+\])+/g, ''); 
+  /* processString */
+  let s = address.readShiftJisString();
+  s = s
+    .replace(/(#n)+/g, " ")
+    .replace(/(#[A-Za-z]+\[(\d*[.])?\d+\])+/g, "")
+    .replace(/\u3000+/gu, "");
 
-    return s;
-    
+  return s;
 }
-
