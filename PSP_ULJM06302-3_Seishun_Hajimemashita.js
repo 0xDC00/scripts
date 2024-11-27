76ca865978e74619f9d487fa248e488e82d01529
kenzy <syren808@gmail.com>
2024-11-27 18:09:26 +0700

    Clean up the script of PSP Seishun Hajimemashita

--- PSP_ULJM06302-3_Seishun_Hajimemashita.js
+++ PSP_ULJM06302-3_Seishun_Hajimemashita.js
@@ -10,17 +10,16 @@ const { setHook } = require("./libPPSSPP.js");
 const mainHandler = trans.send(handler);
 
 setHook({
-    0x880a744: mainHandler.bind_(null, 0, 0), // text
+    0x880a744: mainHandler.bind_(null, 0), // text
 });
 
 function handler(regs, index, offset) {
+    const address = regs[index].value;
     // console.log('onEnter');
     // console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
-    const address = regs[index].value.add(offset);
-    
+        
     let s = address.readShiftJisString();
-    s = s.replace(/(#n)+/g, ' ')
-         .replace(/(#[A-Za-z]+\[(\d*[.])?\d+\])+/g, '')
+    s = s.replace(/\n/g, ''); 
 
-    return s;
+return s;
 }
