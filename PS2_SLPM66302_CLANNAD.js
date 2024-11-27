2787edc48c3dd786fe2d38a9f4a697173dc30550
Logan Lowe <loganr.lowe@gmail.com>
2024-11-27 14:46:07 -0700

    Fix PS2 scripts
    Filtering was bad, Snow was spitting out garbage from the decoder at the end of some lines, also add MC names

--- PS2_SLPM66302_CLANNAD.js
+++ PS2_SLPM66302_CLANNAD.js
@@ -1,26 +1,32 @@
 // ==UserScript==
 // @name         [SLPM66302] CLANNAD
-// @version      0.1
+// @version      0.2
 // @author       logantgt
 // @description  PCSX2 x64
 // * Interchannel/Prototype Legacy Engine
 // ==/UserScript==
 
-const { setHookEE } = require("./libPCSX2.js");
-const { asPsxPtr } = require("./libPCSX2.js");
+const { setHookEE, asPsxPtr } = require("./libPCSX2.js");
 
 setHookEE({
     0x14AC38: trans.send(handler)
 });
 
 function handler(args) {
-        /* processString */
+        // Read string
         let s = this.context.s4(asPsxPtr).readShiftJisString().split(");")[0];
+
+        // Filter out name text
+        if(s.split('】').length > 1) { s = s.split('】')[1] }
+
+        // Process string, replace MC name variables
         s = s
             .replace(/(\\n)+/g, ' ')  
             .replace(/\\d$|^\@[a-z]+|#.*?#|\$/g, '')
             .replace(/\u3000+/gu, '')
-            .replace(/@w|\\c/g, '');
+            .replace(/@w|\\c/g, '')
+            .replace("＊Ａ", "岡崎")
+            .replace("＊Ｂ", "朋也")
     
         return s;
 }
