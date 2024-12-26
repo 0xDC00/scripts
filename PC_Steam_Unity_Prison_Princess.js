a22ace8a494d267c6d243a62df26423fb3e9b4f1
Koukdw <12419106+koukdw@users.noreply.github.com>
2024-12-26 01:09:49 +0100

    Update PC_Steam_Unity_Prison_Princess.js

--- PC_Steam_Unity_Prison_Princess.js
+++ PC_Steam_Unity_Prison_Princess.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Prison Princess
 // @version      1.0
-// @author       Tom (tomrock645) 
+// @author       Tom (tomrock645) with help of Koukdw
 // @description  Steam
 // @developer & publisher: qureate
 //
