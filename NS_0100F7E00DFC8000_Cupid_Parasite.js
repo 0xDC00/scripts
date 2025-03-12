63018d9ca00779c47c753ecc99d6c0d1bc1ecb7c
Mansive <33560917+Mansive@users.noreply.github.com>
2025-03-12 14:18:33 -0500

    Update Cupid Parasite

--- NS_0100F7E00DFC8000_Cupid_Parasite.js
+++ NS_0100F7E00DFC8000_Cupid_Parasite.js
@@ -1,41 +1,43 @@
 // ==UserScript==
-// @name         [0100F7E00DFC8000] Cupid Parasite
+// @name         [0100F7E00DFC8000] Cupid Parasite (キューピット・パラサイト)
 // @version      1.0.1
-// @author       [zooo]
+// @author       [zooo], Mansive
 // @description  Yuzu
 // * Otomate
 // ==/UserScript==
 
-const gameVer = '1.0.1';
-const { setHook } = require('./libYuzu.js');
+const gameVer = "1.0.1";
+const { setHook } = require("./libYuzu.js");
 
-const mainHandler = trans.send(handler, '200+'); // join 200ms
+const mainHandler = trans.send(handler, "200+"); // join 200ms
 
-setHook({
-    '1.0.1': {
-        [0x80057910 - 0x80004000]: mainHandler.bind_(null, 2, "name + text"), 
-        [0x80169df0 - 0x80004000]: mainHandler.bind_(null, 0, "choice"),
-    }
-
-}[globalThis.gameVer = globalThis.gameVer ?? gameVer]);
+setHook(
+  {
+    "1.0.1": {
+      // [0x80057910 - 0x80004000]: mainHandler.bind_(null, 2, "name + text"),
+      [0x80064684 - 0x80004000]: mainHandler.bind_(null, 0, "name"),
+      [0x800640d8 - 0x80004000]: mainHandler.bind_(null, 0, "text monologue"),
+      [0x800646e0 - 0x80004000]: mainHandler.bind_(null, 1, "text dialogue"),
+      [0x80169df0 - 0x80004000]: mainHandler.bind_(null, 0, "choice"),
+    },
+  }[(globalThis.gameVer = globalThis.gameVer ?? gameVer)]
+);
 
 function handler(regs, index, hookname) {
-    const reg = regs[index];
-    console.log('onEnter');
+  const address = regs[index].value;
 
+  console.log("onEnter: " + hookname);
+  //console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
 
-    console.log('onEnter: ' + hookname);
-    const address = reg.value;
-    //console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
+  /* processString */
+  let s = address.readUtf32StringLE();
 
-    /* processString */
-    let s = address.readUtf32StringLE()
-    s = s.replaceAll(/[\s]/g,'');
-    s = s.replaceAll('#KW','')
-    s = s.replaceAll('#C(TR,0xff0000ff)','');
-    s = s.replace(/#P\(.*\)/g,'');
+  // s = s.replaceAll(/[\s]/g, "");
+  s = s.replace(/\n\u3000?/g, ""); // single line without fullwidth whitespace
+  s = s.replaceAll("#KW", "");
+  s = s.replaceAll("#C(TR,0xff0000ff)", "");
+  s = s.replace(/#P\(.*\)/g, "");
+  s = s.trim();
 
-        ;
-    return s;
-
+  return s;
 }
