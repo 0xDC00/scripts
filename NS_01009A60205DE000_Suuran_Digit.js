22cf00dc2a2f802802ecf56e2ed3c137a6990409
Mansive <33560917+Mansive@users.noreply.github.com>
2025-06-14 11:15:12 -0500

    Replace hooks

--- NS_01009A60205DE000_Suuran_Digit.js
+++ NS_01009A60205DE000_Suuran_Digit.js
@@ -12,19 +12,20 @@ globalThis.ARM = true;
 const { setHook } = require("./libYuzu.js");
 
 const mainHandler = trans.send(handler, "200+");
+const choiceHandler = trans.send(handler2, "200+");
 
 setHook(
   {
     "1.0.0": {
+      [0x2388a0 - 0x204000]: mainHandler.bind_(null, 0, "text"),
       [0x2383e8 - 0x204000]: mainHandler.bind_(null, 0, "name"),
-      [0x23840c - 0x204000]: mainHandler.bind_(null, 0, "text"),
-      [0x23fb5c - 0x204000]: mainHandler.bind_(null, 0, "choice"),
+      [0x23840c - 0x204000]: mainHandler.bind_(null, 0, "choice"),
     },
   }[(globalThis.gameVer = globalThis.gameVer ?? gameVer)]
 );
 
 function handler(regs, index, hookname) {
-  console.log("onEnter:", hookname);
+  // console.log("onEnter:", hookname);
 
   const address = regs[index].value;
   // console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
@@ -33,6 +34,17 @@ function handler(regs, index, hookname) {
     .readUtf8String()
     .replace(/\\\u{3000}/gu, "") // single line
     .replace(/#/g, ""); // color
+
+  return s;
+}
+
+function handler2(regs, index, hookname) {
+  // console.log("onEnter:", hookname);
+
+  const address = regs[index].value;
+  // console.log(hexdump(address, { header: false, ansi: false, length: 0x50 }));
+
+  const s = address.readUtf8String().replace(/\\/g, "\n");
 
   return s;
 }
