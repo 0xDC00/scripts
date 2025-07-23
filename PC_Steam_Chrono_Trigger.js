7383c7ffa8d75f1cee251f1b69683fd50ec11e5d
Spongeroberto <spongerobertosquareptantalones@gmail.com>
2025-07-23 21:58:19 +0200

    add hook for skill descriptions via the main menu

--- PC_Steam_Chrono_Trigger.js
+++ PC_Steam_Chrono_Trigger.js
@@ -12,22 +12,33 @@ const __e = Process.enumerateModules()[0];
 const handler = trans.send(s => s, '100+');
 
 (function () {
-    const pattern = '55 8B ?? 8B ?? ?? 53 8B ?? ?? 56 8B ?? C7';
-    const results = Memory.scanSync(__e.base, __e.size, pattern);
-    if (results.length === 0) {
-        console.error('[DialoguePattern] Hook not found!');
-        return;
-    }
+    attach('DialoguePattern', '55 8B ?? 8B ?? ?? 53 8B ?? ?? 56 8B ?? C7', 'eax');
+    attach('SkillMenu', '42 6A ?? E8 ?? ?? ?? ?? 83 C4 04 C7 45 ?? 0F 00 00 00 C7 45 ?? 00 00 00 00 C6 45 ?? ?? 85 DB 74 ?? 8B 95', 'ecx');
 
-    const address = results[0].address;
-    console.log('[DialoguePattern] Found hook', address);
-    Interceptor.attach(address, function (args) {
-        const text = this.context.eax.readUtf8String()
-            .replaceAll('\\', '')
-            .replace(/\s+/g, '')
-            .replace(/<C[0-9]>.+?<\/C[0-9]>/g, '')
-            .replaceAll('<CT>', '')
-            .replaceAll('<PAGE>', '\n');
-        handler(text);
-    });
+    function attach(name, pattern, register) {
+        const results = Memory.scanSync(__e.base, __e.size, pattern);
+        if (results.length === 0) {
+            console.error(`[${name}] Hook not found!`);
+            return;
+        }
+
+        console.log(`[${name}] Found ${results.length} matches`);
+
+        const address = results[0].address;
+        console.log(`[${name}] Found hook`, address);
+        Interceptor.attach(address, function (args) {
+            const text = this.context[register].readUtf8String()
+                .replaceAll('\\', '')
+                .replace(/\s+/g, '')
+                .replace(/<C[0-9]>.+?<\/C[0-9]>/g, '')
+                .replaceAll('<CT>', '')
+                .replaceAll('<PAGE>', '\n');
+            handler(text);
+        });
+    }
 })();
+
+
+
+
+
