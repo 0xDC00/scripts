0511b515d780b70be19999ff3ae984158c74ab5c
Lucasa <71572681+Lucasap33@users.noreply.github.com>
2026-02-01 18:51:40 -0300

    Add Dragon Ball Heroes Ultimate Mission Trilogy (#989)

---
+++ 3DS_Japan_Dragon_Ball_Heroes_Ultimate_Mission_2.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         [0004000000120600] Dragon Ball Heroes: Ultimate Mission 2
+// @version      1.0
+// @author       [Lucasap33]
+// @description  Citra
+// ==/UserScript==
+const { setHook } = require("./libCitra.js");
+
+const mainHandler = trans.send(handler.bind_(null, 5), '200+'); // join 200ms
+
+setHook({
+    0x345644: mainHandler, // dialogue only
+});
+
+function handler(regs, index) {
+    const address = regs[index].value;
+
+    //console.log('onEnter');
+
+    /* processString */
+    let s = address.readUtf8String();
+
+    // filters
+    s = s
+		.replace(/\[(.+?)\]/g, '$1') // furigana
+		.replace(/\{(.+?)\}/g, '($1)') // furigana
+        ;
+
+    return s;
+}
