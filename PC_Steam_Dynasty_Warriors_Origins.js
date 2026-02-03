1f7bee56d16bd7968de6be75fce79d40eb1bcf7f
Musi57 <254671545+Musi57@users.noreply.github.com>
2026-02-03 18:15:34 +0100

    update dialogue patterns to be more robust

--- PC_Steam_Dynasty_Warriors_Origins.js
+++ PC_Steam_Dynasty_Warriors_Origins.js
@@ -15,21 +15,22 @@ console.warn('[Known Issue] When opening the log in game, the line at the top is
 console.warn('[Known Issue] On choices, only the first option is output, this is a limit of the DialogueHook');
 
 (function () {
-    attach('FullscreenMovieText', 'E8 22 8A 1A 00', 'rdx');
-    attach('FullscreenText', 'E8 F8 7A 2A 00', 'rdx');
-    attach('BattleTextboxDialogue', 'E8 80 83 1A 00', 'rdx');
-    attach('BattleDialogue', 'E8 F7 F9 6F 00', 'rdx');
-    attach('MovieDialogue', 'E8 D1 A5 D9 00', 'rdx');
-    attach('TutorialHook', 'E8 D7 90 40 00', 'rdx');
-    attach('DialogueHook', 'E8 26 89 40 00', 'rdx');
+    attach('FullscreenMovieText', 'E8 22 8A 1A 00', 'rdx', 0); // i wasnt able to find a robust hook that works across versions on this
+    attach('FullscreenText', 'E8 F8 7A 2A 00', 'rdx', 0); // same here
+    attach('BattleTextboxDialogue', '49 8B CA C6 44 24 20 01 E8 ?? ?? ?? ?? 48 83 C4 58', 'rdx', 8);
+    attach('BattleDialogue', 'C6 44 24 20 00 41 B1 01 E8 ?? ?? ?? ?? 48 8B 4F 60', 'rdx', 8);
+    attach('MovieDialogue', 'C6 44 24 28 01 C6 44 24 20 01 E8 ?? ?? ?? ?? 49 8B 8D 90', 'rdx', 10);
+    attach('TutorialHook', '44 88 4C 24 20 41 B1 01 E8 ?? ?? ?? ?? 48 83 C4 58 C3 CC CC 4C', 'rdx', 8);
+    attach('DialogueHook', '44 88 4C 24 20 41 B1 01 E8 ?? ?? ?? ?? 48 83 C4 58 C3 CC 33 C0', 'rdx', 8);
 
-    function attach(name, pattern, register) {
+    function attach(name, pattern, register, offset) {
         const results = Memory.scanSync(__e.base, __e.size, pattern);
         if (results.length === 0) {
             console.error(`[${name}] Hook not found!`);
             return;
         }
-        const address = results[0].address;
+        
+        const address = results[0].address.add(offset);
         console.log(`[${name}] Found hook at ${address}`);
         let previous = '';
 
@@ -44,13 +45,13 @@ console.warn('[Known Issue] On choices, only the first option is output, this is
                     previous = rawText;
 
                     const cleanedText = rawText
-                        .replace(/[p]/g, '無名') // replace protag name
-                        .replace(/[ft].?[fe]/g, '')
-                        .replace(/[fs]|[/]/g, '')
-                        .replace(/[$.?]/g, '')
-                        .replace(/[c\d+]/g, '')
-						.replace(/\[t\].*?\[\]/g, '')
-						.replace(/\[\]/g, '')
+                        .replace(/\[p\]/g, '無名') // replace protag name
+                        .replace(/\[ft\].*?\[fe\]/g, '')
+                        .replace(/\[fs\]|\[\/\]/g, '')
+                        .replace(/\[\$.*?\]/g, '')
+                        .replace(/\[c\d+\]/g, '')
+                        .replace(/\[t\].*?\[\]/g, '')
+                        .replace(/\[\]/g, '')
                         .trim();
 
                     if (cleanedText) {
