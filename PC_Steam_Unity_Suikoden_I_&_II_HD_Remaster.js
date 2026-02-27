24f80cef1ea7df2e59f10fd34bc1837264c15de6
Musi <254671545+Musi57@users.noreply.github.com>
2026-02-27 02:03:20 +0100

    Add Suikoden I&II HD Remaster Gate Rune and Dunan Unification Wars (#1009)
    * add suikoden remaster
    
    * fix typo
    
    Co-authored-by: Mansive <33560917+Mansive@users.noreply.github.com>
    
    * refine logic to use object names and cache unity methods
    
    * resolve get_name from UnityEngine.Object
    
    * refactor to use args[0].wrap().get_name().readMonoString()
    
    ---------
    
    Co-authored-by: Mansive <33560917+Mansive@users.noreply.github.com>

---
+++ PC_Steam_Unity_Suikoden_I_&_II_HD_Remaster.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         Suikoden I&II HD Remaster Gate Rune and Dunan Unification Wars
+// @version      1.0.4
+// @author       Musi
+// @description  Steam
+// * KONAMI
+//
+// https://store.steampowered.com/app/1932640/Suikoden_III_HD_Remaster_Gate_Rune_and_Dunan_Unification_Wars/
+// ==/UserScript==
+
+const Mono = require('./libMono.js');
+const handleLine = trans.send((s) => s, '250+');
+
+console.warn('[Known issue] You may have to detach from the game manually when exiting, otherwise it might not close.\n');
+
+let currentLines = {};
+let flushTimer = null;
+const FLUSH_DELAY = 250; // this was tested with dialogue set to fast, adjust as necessary
+
+Mono.perform(() => {
+    Mono.setHook('Unity.TextMeshPro', 'TMPro.TextMeshProUGUI', 'set_text', -1, {
+        onEnter(args) {
+            // return early if pointer is null
+            const textPtr = args[1];
+            if (!textPtr || textPtr.isNull()) return;
+
+            // get unity object name to filter dialogue lines
+            const name = args[0].wrap().name.readMonoString();
+
+            // whitelist dialogue objects
+            if (!name.startsWith('Txt_Command0')) return;
+
+            let update = textPtr.readMonoString();
+            if (!update) return;
+
+            // map text to its specific object name to handle multi-line windows
+            currentLines[name] = update;
+
+            // remove alignment tags
+            clearTimeout(flushTimer);
+            flushTimer = setTimeout(() => {
+                // combine lines 1, 2, and 3 in order
+                let fullText = Object.keys(currentLines).sort().map(k => currentLines[k]).join('\n');
+                let cleanOutput = fullText.replace(/<[^>]*>/g, '').trim();
+                
+                if (cleanOutput.length > 0) {
+                    handleLine(cleanOutput);
+                }
+                
+                currentLines = {};
+             }, FLUSH_DELAY);
+        }
+    });
+});
