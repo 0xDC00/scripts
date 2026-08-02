a84d9f5dbee97550d7e498d7c92da56235a0b5ab
Mansive <33560917+Mansive@users.noreply.github.com>
2026-08-02 02:04:34 -0500

    Hook name in onEnter log

--- PC_Steam_Unity_Touhou_Hero_of_Ice_Fairy.js
+++ PC_Steam_Unity_Touhou_Hero_of_Ice_Fairy.js
@@ -14,7 +14,7 @@ const handler = trans.send((s) => s, "200+");
 
 Mono.setHook("", "DialogueMgr", "UpdateContent", 2, {
     onEnter(args) {
-        console.log("onEnter");
+        console.log("onEnter: DialogueMgr.UpdateContent");
         const text = args[1].readMonoString();
         console.warn(JSON.stringify(text));
         handler(text);
