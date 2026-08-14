b309f9fea4deadbfae1d4fd740c65d888a3f92de
Daisouji <82060898+Daisouji@users.noreply.github.com>
2026-08-14 00:54:35 -0400

    Adding 3DS_Japan_Fire_Emblem_IF_Special_Edition.js

---
+++ 3DS_Japan_Fire_Emblem_IF_Special_Edition.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         [000400000012DE00] Fire Emblem: IF Special
+// @version      1.1
+// @author       [Daisouji]
+// @description  Citra
+// ==/UserScript==
+const { setHook } = require("./libCitra.js");
+
+const mainHandler = trans.send(handler.bind_(null, 1), '200+'); // join 200ms
+
+setHook({ 
+     0x190af4: mainHandler, // dialogue only
+});
+
+function handler(regs, index) {
+    const address = regs[index].value;
+
+    //console.log('onEnter');
+
+    /* processString */
+    let s = address.readUtf16String();
+    
+    
+    if(s.includes('$')){
+    let lastindex = s.indexOf('$')
+    s = s.substring(0,lastindex);
+    }
+
+
+    s=s
+    .replaceAll("_"," ")
+ 
+
+    return s;
+}
+
+//大丈夫か、カムイ_ぼんやりしていたようだが
